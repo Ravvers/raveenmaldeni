ac7c925da98b91eef43fef404061da0cbf76dd77
Raveen <rmaldeni@gmail.com>
2023-06-15 23:46:14 +1000

    add colour palette

---
+++ src/styles/colourPalette.ts
@@ -0,0 +1,17 @@
+export const colourPalette = {
+	lightBlue: "#30BCED",
+	gray: "D9D9D9",
+	black: "000000",
+	white: "FFFAFF"
+};
+
+export const theme = {
+	body: {
+		background: colourPalette.lightBlue,
+		text: colourPalette.white
+	},
+	footer: {
+		text: colourPalette.black,
+		background: colourPalette.gray
+	}
+};
