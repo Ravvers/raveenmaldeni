a499d29fa6f764a7d989fb729d9fdd6742abef96
Raveen <rmaldeni@gmail.com>
2023-06-25 21:22:58 +1000

    add blog page content

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { PageTitle } from "../../components/PageTitle/PageTitle";
 
 export const Blog = () => {
-	return <h1>Blog</h1>;
+	return (
+		<>
+			<PageTitle>
+				<h1>Teach to learn</h1>
+			</PageTitle>
+			<p>
+				I use this space to consolidate my thoughts and ideas about a
+				variety of topics. Hopefully my perspective helps you form your
+				own or learn something new.
+			</p>
+			<p>I am still working on the first blog post, come back soon!</p>
+		</>
+	);
 };
